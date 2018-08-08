d2b95a98b7df0547e333177eb373e5b5e9d93704
enrifransch <enrifransch@gmail.com>
2018-08-08 18:13:29 -0500

    funcion para poner en no disponible briefs si no han sido actualizados en las ultimas 2 horas

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,58 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+
 admin.initializeApp();
 
 export const onBriefStatusChange = functions.database
-  .ref('brief/{briefId}')
-  .onUpdate(change => {
-    return new Promise((resolve, reject) => {
-      const after = change.after
+    .ref('brief/{briefId}')
+    .onUpdate(change => {
+        return new Promise((resolve, reject) => {
+            const after = change.after
 
-      if (after.val() === 'true') {
-        resolve();
-      }
+            if (after.val() === 'true') {
+                resolve();
+            }
 
-      return admin.firestore()
-        .collection('brief')
-        .doc(after.key)
-        .update({ available: false })
-    })
-      .catch(console.error);
-  })
+            return admin.firestore()
+                .collection('brief')
+                .doc(after.key)
+                .update({available: false})
+        })
+            .catch(console.error);
 
+    });
+
+export const onUpdateActiveStatus = functions.https
+    .onRequest((request, response) => {
+        admin.firestore().collection('brief')
+            .get()
+            .then(function (querySnapshot) {
+                const arr = []
+                const promiseList = []
+                querySnapshot.forEach((doc) => {
+                    const id = doc.id;
+                    const now = Date.now() - 7200000;
+                    if (now > doc.data().lastUpdate) {
+                        if (doc.data().available) {
+                            promiseList.push(admin.firestore().collection('brief').doc(id).update({available: false}));
+                            arr.push(id)
+                        }
+                    }
+                });
+                if (promiseList.length > 0) {
+                    Promise.all(promiseList)
+                        .then(res => {
+                            response.send({message: 'Documents updated', documents: arr})
+                        })
+                        .catch(error => {
+                            response.status(500).send(error)
+                        })
+                }
+                else {
+                    response.send({message: 'No documents updated'})
+                }
+            })
+            .catch(function (error) {
+                response.status(500).send(error)
+            });
+    });
